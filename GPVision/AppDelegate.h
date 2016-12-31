8b12d7c50b42582cd1fbe670bb53afd78d95f745
王涛 <wt0110@126.com>
2016-12-31 12:40:50 +0800

    Initial Commit

---
+++ GPVision/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPVision
+//
+//  Created by wangtao on 2016/12/31.
+//  Copyright © 2016年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPVision/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPVision
+//
+//  Created by wangtao on 2016/12/31.
+//  Copyright © 2016年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
